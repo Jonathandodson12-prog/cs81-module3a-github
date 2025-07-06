9fd89ea7f4f5d067ee03665a83ed2e08c28df9c7
Jonathan Dodson <jonathandodson12@outlook.com>
2025-07-05 23:45:17 -0500

    Add isPalindrome() function

--- journal.js
+++ journal.js
@@ -6,4 +6,9 @@ function greet(name) {
 function isEven(num) {
   return num % 2 === 0;
 }
+// Check if a word is a palindrome
+function isPalindrome(word) {
+  let cleaned = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleaned === cleaned.split('').reverse().join('');
+}
 
